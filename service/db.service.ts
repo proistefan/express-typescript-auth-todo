5cc8aa45faa4c05101d0673d00a787358c6d6c94
proistefan <i.stefan.pm@gmail.com>
2022-05-07 13:58:04 +0500

    add error db log

--- service/db.service.ts
+++ service/db.service.ts
@@ -2,8 +2,10 @@ import asyncFs from 'fs/promises'
 import fs from 'fs'
 import {IDb} from "../type/db";
 import {categories, categoryItems, categoryProductList, products} from "../data";
+import {EOL} from "os";
 
 const dbPath = './db.json'
+const loggerPath = './logger/log.txt'
 const initialDb: IDb = {
   users: [],
   tokens: [],
@@ -17,11 +19,21 @@ const initialDb: IDb = {
 
 class DbService {
   static async read(): Promise<IDb> {
-    if (!fs.existsSync(dbPath)) {
+    try {
+      if (!fs.existsSync(dbPath)) {
+        await DbService.create()
+      }
+      const db = await asyncFs.readFile(dbPath)
+      return JSON.parse(db.toString())
+    } catch (e) {
+      await DbService.setError(e)
+      
+      fs.unlinkSync(dbPath);
       await DbService.create()
+      
+      const db = await asyncFs.readFile(dbPath)
+      return JSON.parse(db.toString())
     }
-    const db = await asyncFs.readFile(dbPath)
-    return JSON.parse(db.toString())
   }
 
   static async write(data: IDb) {
@@ -30,6 +42,16 @@ class DbService {
 
   static async create() {
     await asyncFs.writeFile(dbPath, JSON.stringify(initialDb))
+  }
+  
+  static async setError(e: any) {
+    const message = `${new Date().toString()} ${JSON.stringify(e && e.message || 'Unknown error')} ${EOL}`
+    
+    if (!fs.existsSync(loggerPath)) {
+      await asyncFs.writeFile(loggerPath, message)
+    } else {
+      await asyncFs.appendFile(loggerPath, message)
+    }
   }
 }
 
